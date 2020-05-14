7951fc114a9390c277b9e1e4e62fac6e51df08b3
david <davidealvarez1@gmail.com>
2020-05-14 12:27:32 -0700

    added lecture code for knex migrations

---
+++ data/migrations/20200514102428_init.js
@@ -0,0 +1,60 @@
+exports.up = function (knex) {
+    return knex.schema
+      .createTable("suppliers", suppliers => {
+        // suppliers.uuid('id').primary();
+        suppliers.increments();
+  
+        suppliers.string("name", 255).notNullable();
+      })
+      .createTable("shippers", shippers => {
+        shippers.increments();
+      })
+      .createTable("clients", clients => {})
+      .createTable("products", products => {})
+      .createTable("orders", orders => {
+        orders.increments();
+  
+        orders
+          .integer("client")
+          .unsigned()
+          .notNullable()
+          .references("id") // or .references('clients.id') then remove .inTable()
+          .inTable("clients")
+          .onUpdate("CASCADE") // RESTRICT, DO NOTHING, SET NULL, CASCADE
+          .onDelete("RESTRICT");
+      })
+      .createTable("order_shippers", orderShippers => {
+        orderShippers.increments();
+  
+        orderShippers
+          .integer("order")
+          .unsigned()
+          .notNullable()
+          .references("id") // or .references('orders.id') then remove .inTable()
+          .inTable("orders")
+          .onUpdate("CASCADE") // RESTRICT, DO NOTHING, SET NULL, CASCADE
+          .onDelete("RESTRICT");
+  
+        orderShippers
+          .integer("shipper")
+          .unsigned()
+          .notNullable()
+          .references("id") // or .references('shippers.id') then remove .inTable()
+          .inTable("shippers")
+          .onUpdate("CASCADE") // RESTRICT, DO NOTHING, SET NULL, CASCADE
+          .onDelete("RESTRICT");
+      })
+      .createTable("order_products", orderProducts => {});
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema
+      .dropTableIfExists("order_products")
+      .dropTableIfExists("order_shippers")
+      .dropTableIfExists("orders")
+      .dropTableIfExists("products")
+      .dropTableIfExists("clients")
+      .dropTableIfExists("shippers")
+      .dropTableIfExists("suppliers");
+  };
+  
